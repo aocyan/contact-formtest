645f617470a7b642d2263726b4c6151830a0fd29
aocyan <habacchi.1008@gmail.com>
2025-01-26 13:59:50 +0900

    [add]thanks画面の作成

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>FashionablyLate</title>
+	<link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+</head>
+
+<body>
+
+<main>
+	<div class="thanks__inner">
+        <div class="thanks__logo">
+		    Thank you
+        </div>
+	</div>
+    <div class="confirm-thanks">
+        <div class="confirm-thanks--item">
+            お問い合わせありがとうございました
+        </div>
+    </div>
+    <div class="home__link">
+        <a class="home__link--item" href="/">Home</a>
+    </div>
+</main>
+
+</body>
+
+
+</html>
