4b38de6dd789d97afa341ba4358ffd30c297163b
aocyan <habacchi.1008@gmail.com>
2025-01-27 19:39:57 +0900

    [feat]AuthControllerでindexファイルが表示されるように設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [ContactController::class, 'index']);
+Route::get('/', [AuthController::class, 'index']);
+
 Route::get('/confirm', [ContactController::class, 'confirm']);
 Route::get('/thanks', [ContactController::class, 'thanks']);
 Route::get('/admin', [ContactController::class, 'admin']);
